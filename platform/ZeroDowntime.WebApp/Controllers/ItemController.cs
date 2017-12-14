32f6018ac0975f19177279ea882008d9999e293b
Shraddha <shraddha@avyanconsulting.com>
2017-12-14 16:42:47 +0530

    dev changes

---
+++ platform/ZeroDowntime.WebApp/Controllers/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroDowntime.Models;
+
+namespace ZeroDowntime.WebApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Create")]
+        public ActionResult CreateAsync()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateAsync([Bind(Include = "Id,Name,Description")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+    }
+}
+
